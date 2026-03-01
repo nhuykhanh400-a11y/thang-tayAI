7008d3cee0450a43f85bb890abdf4dc53b14d921
nhuykhanh400-a11y <nhuykhanh400@gmail.com>
2026-03-01 17:33:45 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
      body: JSON.stringify({
-  model: "llama-3.1-8b-instant",
+  model: "llama-3.1-70b-versatile",
   temperature: 0.35,
   top_p: 0.4,
 messages: [
