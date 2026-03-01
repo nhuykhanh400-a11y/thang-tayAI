58763632f3eac64d93036770473fa0c709aef4e5
nhuykhanh400-a11y <nhuykhanh400@gmail.com>
2026-03-02 01:16:23 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -40,7 +40,7 @@ function needSearch(question) {
   );
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(200).json({ message: "API is working. Use POST." });
   }
